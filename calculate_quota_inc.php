fb78d8f796d4bb7f2127dc8da2b61d0a9f582d10
Max Kremmel <xing@synapse.plus.com>
2006-06-10 16:18:42 +0000

    move uploadMax calculation to seperate file for easy inclusion

---
+++ calculate_quota_inc.php
@@ -0,0 +1,20 @@
+<?php
+require_once( QUOTA_PKG_PATH.'LibertyQuota.php' );
+
+$quota = new LibertyQuota();
+if( !$gBitUser->isAdmin() && !$quota->isUserUnderQuota( $gBitUser->mUserId ) ) {
+	$gBitSystem->display( 'bitpackage:quota/over_quota.tpl', tra( 'You are over your quota.' ) );
+	die;
+}
+
+if( !$gBitUser->isAdmin() ) {
+	// Prevent people from uploading more than their quota
+	$q = $quota->getUserQuota( $gBitUser->mUserId );
+	$u = $quota->getUserUsage( $gBitUser->mUserId );
+	$gBitSmarty->assign( 'quotaMessage', tra( 'Your remaining disk quota is' ).' '.round( ( $q - $u ) / 1000000, 2 ).' '.tra( 'Megabytes' ) );
+	$qMegs = round( $q / 1000000 );
+	if( $qMegs < $uploadMax ) {
+		$uploadMax = $qMegs;
+	}
+}
+?>
