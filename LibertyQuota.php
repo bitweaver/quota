b0ac2ae368d2f21fa8bf2e9e88905b059c999703
Christian Fowler <spider@viovio.com>
2006-09-05 21:12:59 +0000

    remove debug bt

--- LibertyQuota.php
+++ LibertyQuota.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_quota/LibertyQuota.php,v 1.11 2006/08/28 14:50:47 sylvieg Exp $
+ * $Header: /cvsroot/bitweaver/_bit_quota/LibertyQuota.php,v 1.12 2006/09/05 21:12:59 spiderr Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,7 +8,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: LibertyQuota.php,v 1.11 2006/08/28 14:50:47 sylvieg Exp $
+ * $Id: LibertyQuota.php,v 1.12 2006/09/05 21:12:59 spiderr Exp $
  * @package quota
  */
 
@@ -28,7 +28,7 @@ require_once( LIBERTY_PKG_PATH.'LibertyAttachable.php' );
  *
  * @author spider <spider@steelsun.com>
  *
- * @version $Revision: 1.11 $ $Date: 2006/08/28 14:50:47 $ $Author: sylvieg $
+ * @version $Revision: 1.12 $ $Date: 2006/09/05 21:12:59 $ $Author: spiderr $
  */
 class LibertyQuota extends LibertyBase {
     /**
@@ -173,7 +173,6 @@ class LibertyQuota extends LibertyBase {
     * returns the quota and consumption if a user is under usage level
 	**/
 	function isUserUnderQuota( $pUserId ) {
-bt();
 		$ret = FALSE;
 		if( is_numeric( $pUserId ) ) {
 			$query = 'SELECT MAX(qo.`disk_usage`) AS `disk_usage`
